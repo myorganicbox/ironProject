d145f10f1cae17488fdebbbaacb8112d62506f31
Dimitri.Bonnier <dimitri.bonnierf@gmail.com>
2018-02-27 16:33:38 -0600

    l

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const Basket = require('../models/Basket');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Basket.find()
+  .then(docs=>{
+    res.render('viewname', {baskets:docs});
+  })
 });
 
 module.exports = router;
