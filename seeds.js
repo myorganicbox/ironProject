01d01da0f7fc7da81413faf1172dbcf58392025b
Andrea <andreahackerpage@gmail.com>
2018-02-27 17:55:26 -0600

    styles

--- seeds.js
+++ seeds.js
@@ -78,23 +78,23 @@ const meals = [
     image: ["https://cdn.shopify.com/s/files/1/1044/2600/products/IMG_9960_grande.jpg?v=1489304053"]
         },
     {
-    name: "Smoky Chickpeas & Quinoa",
-    description: "Chickpeas in a smoky, mildly spiced tomato sauce, with quinoa and garnished with fresh coriander." ,
+    name: "Ruby Quinoa and lentil salad    ",
+    description: "Shredded beetroot and carrots with red quinoa, puy lentils, fresh mint and coriander, with a sesame oil and rice vinaigrette, garnished with slivered almonds." ,
     image: ["https://cdn.shopify.com/s/files/1/1044/2600/products/IMG_2622_grande.jpg?v=1519550570"]
         },
     {
-    name: "Pad Thai with Jackfruit",
-    description: "A Thai rice noodle dish with carrots, bean sprouts, shallots and roasted shredded jackfruit, in a tamarind and lime sauce, topped with crushed peanuts, fresh coriander, spring onions and red chilli.." ,
+    name: "Roasted Red Capsicum Pasta",
+    description: "Spaghetti in a creamy sauce made by blending cashews and roasted red capsicums together, with nutritional yeast and chilli flakes." ,
     image: ["https://cdn.shopify.com/s/files/1/1044/2600/products/IMG_1690_grande.jpg?v=1497784729"]
         },
     {
-    name: "Biryani",
-    description: "This south Asian dish is made with basmati rice, red capsicums, tofu, peas and all the traditional Indian spices such as garam masala, turmeric and cumin seeds, garnished with golden raisins. ." ,
+    name: "Chickpea Tikka Massala",
+    description: "A tikka masala sauce made with tomatoes, coconut cream, kumara and Indian spices, served with brown rice and fresh coriander." ,
     image: ["https://cdn.shopify.com/s/files/1/1044/2600/products/IMG_9904_grande.jpg?v=1489303829"]
       },
     {
-    name: "Burritos",
-    description: "A family favourite that is super quick to make, multigrain wraps filled with refried beans, brown rice, corn, lettuce, tomatoes and guacamole." ,
+    name: "Portobello Fajita Bowl",
+    description: "Smoky Quinoa, topped with pan fried chipotle spiced red and yellow capsicums, portobello mushrooms, red onions and baby spinach, drizzled with a cashew sour cream and a lime wedge to squeeze." ,
     image: ["https://cdn.shopify.com/s/files/1/1044/2600/products/IMG_9960_grande.jpg?v=1489304053"]
       },
         
