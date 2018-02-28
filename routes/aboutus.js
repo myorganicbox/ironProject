de9a09269835e2eeb07a71ecea7681dd1e361954
Andrea <andreahackerpage@gmail.com>
2018-02-28 09:59:08 -0600

    home

--- routes/aboutus.js
+++ routes/aboutus.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router  = express.Router();
 
 /* GET home page. */
-router.get('/aboutus', (req, res, next) => {
-  res.render('about');
+router.get('/', (req, res, next) => {
+  res.render('aboutus');
 });
 
 module.exports = router;
