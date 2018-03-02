a953152c3860f70494c97be242be7b8e4fb0e35e
Andrea <andreahackerpage@gmail.com>
2018-03-02 09:14:25 -0600

    done

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -100,64 +100,64 @@ function payWithStripe(e) {
 // $('input[name=cardCVC]').payment('formatCardCVC');
 // $('input[name=cardExpiry').payment('formatCardExpiry');
 
-/* Form validation using Stripe client-side validation helpers */
-jQuery.validator.addMethod("cardNumber", function(value, element) {
-    return this.optional(element) || Stripe.card.validateCardNumber(value);
-}, "Please specify a valid credit card number.");
+// /* Form validation using Stripe client-side validation helpers */
+// jQuery.validator.addMethod("cardNumber", function(value, element) {
+//     return this.optional(element) || Stripe.card.validateCardNumber(value);
+// }, "Please specify a valid credit card number.");
 
-jQuery.validator.addMethod("cardExpiry", function(value, element) {    
-    /* Parsing month/year uses jQuery.payment library */
-    value = $.payment.cardExpiryVal(value);
-    return this.optional(element) || Stripe.card.validateExpiry(value.month, value.year);
-}, "Invalid expiration date.");
+// jQuery.validator.addMethod("cardExpiry", function(value, element) {    
+//     /* Parsing month/year uses jQuery.payment library */
+//     value = $.payment.cardExpiryVal(value);
+//     return this.optional(element) || Stripe.card.validateExpiry(value.month, value.year);
+// }, "Invalid expiration date.");
 
-jQuery.validator.addMethod("cardCVC", function(value, element) {
-    return this.optional(element) || Stripe.card.validateCVC(value);
-}, "Invalid CVC.");
+// jQuery.validator.addMethod("cardCVC", function(value, element) {
+//     return this.optional(element) || Stripe.card.validateCVC(value);
+// }, "Invalid CVC.");
 
-validator = $form.validate({
-    rules: {
-        cardNumber: {
-            required: true,
-            cardNumber: true            
-        },
-        cardExpiry: {
-            required: true,
-            cardExpiry: true
-        },
-        cardCVC: {
-            required: true,
-            cardCVC: true
-        }
-    },
-    highlight: function(element) {
-        $(element).closest('.form-control').removeClass('success').addClass('error');
-    },
-    unhighlight: function(element) {
-        $(element).closest('.form-control').removeClass('error').addClass('success');
-    },
-    errorPlacement: function(error, element) {
-        $(element).closest('.form-group').append(error);
-    }
-});
+// validator = $form.validate({
+//     rules: {
+//         cardNumber: {
+//             required: true,
+//             cardNumber: true            
+//         },
+//         cardExpiry: {
+//             required: true,
+//             cardExpiry: true
+//         },
+//         cardCVC: {
+//             required: true,
+//             cardCVC: true
+//         }
+//     },
+//     highlight: function(element) {
+//         $(element).closest('.form-control').removeClass('success').addClass('error');
+//     },
+//     unhighlight: function(element) {
+//         $(element).closest('.form-control').removeClass('error').addClass('success');
+//     },
+//     errorPlacement: function(error, element) {
+//         $(element).closest('.form-group').append(error);
+//     }
+// });
 
-paymentFormReady = function() {
-    if ($form.find('[name=cardNumber]').hasClass("success") &&
-        $form.find('[name=cardExpiry]').hasClass("success") &&
-        $form.find('[name=cardCVC]').val().length > 1) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// paymentFormReady = function() {
+//     if ($form.find('[name=cardNumber]').hasClass("success") &&
+//         $form.find('[name=cardExpiry]').hasClass("success") &&
+//         $form.find('[name=cardCVC]').val().length > 1) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-$form.find('[type=submit]').prop('disabled', true);
-var readyInterval = setInterval(function() {
-    if (paymentFormReady()) {
-        $form.find('[type=submit]').prop('disabled', false);
-        clearInterval(readyInterval);
-    }
-}, 250);
+// $form.find('[type=submit]').prop('disabled', true);
+// var readyInterval = setInterval(function() {
+//     if (paymentFormReady()) {
+//         $form.find('[type=submit]').prop('disabled', false);
+//         clearInterval(readyInterval);
+//     }
+// }, 250);
 
 
 /*
